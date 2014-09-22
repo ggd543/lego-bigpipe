20a6a5d1c02d7efcde79fd665c7187c70eccae26
Hinc Liu <hincubus@gmail.com>
2014-09-22 10:31:16 +0800

    代码微调，移除无用代码，logger 加入 NODE_ENV 环境变量判断

--- middleware/router.js
+++ middleware/router.js
@@ -1,10 +1,6 @@
 'use strict';
 
 var router = require('express').Router();
-var reactor = require('../lib/reactor');
-var error = require('../lib/error');
-var app = require('../index');
-var logger = app.get('logger') || console;
 
 router.post('/view', require('../api/view'));
 router.post('/unit', require('../api/unit'));
