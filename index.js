9729b062781a981fd2c4caad208a7f3cae741b30
Hinc Liu <hincubus@gmail.com>
2014-09-26 17:35:00 +0800

    调整代码格式

--- index.js
+++ index.js
@@ -165,8 +165,8 @@ function genUrl(ids, ext, config) {
 
 // from jqMobi
 function uuid() {
-    function s4() {
-        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  function s4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
